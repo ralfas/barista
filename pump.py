065421aedca574295144c39a75108e6c1711ebbb
Ralfas <ralfas@gmail.com>
2017-01-14 10:03:05 +0000

    Tweaks

--- pump.py
+++ pump.py
@@ -13,7 +13,7 @@ class Pump:
 	in1_pin = 4
 	in2_pin = 17
 	stop_duty_cycle = 0
-	default_duty_cycle = 80#Minimum ~50 for opperation
+	default_duty_cycle = 100#Minimum ~50 for opperation
 	default_frequency = 500#Hz
 
 
@@ -30,23 +30,29 @@ class Pump:
 
 
 	def forward(self):
+		print 'telling the pump to run forward'
+
+		self.pwm.stop()
 		io.output(Pump.in1_pin, io.HIGH)
 		io.output(Pump.in2_pin, io.LOW)
-		self.pwm.ChangeDutyCycle(Pump.default_duty_cycle)
+		self.pwm.start(Pump.default_duty_cycle)
 		self.state = 'running'
 
 	 
 	def reverse(self):
+		print 'telling the pump to run in reverse'
+
+		self.pwm.stop()
 		io.output(Pump.in1_pin, io.LOW)
 		io.output(Pump.in2_pin, io.HIGH)
-		self.pwm.ChangeDutyCycle(Pump.default_duty_cycle)
+		self.pwm.start(Pump.default_duty_cycle)
 		self.state = 'reverse'
 
 
 	def stop(self):
+		self.pwm.stop()
 		io.output(Pump.in1_pin, io.LOW)
 		io.output(Pump.in2_pin, io.LOW)
-		self.pwm.ChangeDutyCycle(Pump.stop_duty_cycle)
 		self.state = 'stopped'
 
 
@@ -57,19 +63,13 @@ class Pump:
 	def dispense(self, ml):
 		print 'telling the pump to dispense %s' % (ml)
 
-		self.pwm.ChangeDutyCycle(self._ml_to_duty_cycle(ml))
+		self.pwm.ChangeDutyCycle(Pump.default_duty_cycle)
 		sleep(self._sleep_for_ml(ml))
 		self.pwm.ChangeDutyCycle(Pump.stop_duty_cycle)
 
 
 	def cleanup(self):
 		io.cleanup()
-
-
-	def _ml_to_duty_cycle(self, ml):
-		"""How fast to go to dispense ml"""
-
-		return 0
 
 
 	def _sleep_for_ml(self, ml):
